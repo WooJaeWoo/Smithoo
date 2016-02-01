08a259cef32d03262532c7f1d710399b76ad23c6
WooJaeWoo <gewoose@gmail.com>
2016-02-01 11:19:55 +0000

    Set card js work

--- webapp/routes/card.js
+++ webapp/routes/card.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
-	res.render(config.root.VIEW_ROOT);
+	res.render(config.root.VIEW_ROOT + "/card/card");
+});
+
+router.get("/jimin", function(req, res, next) {
+	res.render(config.root.VIEW_ROOT + "/card/jimin");
 });
 
 module.exports = router;
