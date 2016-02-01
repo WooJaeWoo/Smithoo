ce0c914768e73fdbbbd69855e01d7fbe0c55b1f0
WooJaeWoo <gewoose@gmail.com>
2016-02-01 10:39:15 +0000

    Delete logger test

--- setting/app.js
+++ setting/app.js
@@ -31,7 +31,6 @@ module.exports = function(app) {
 	
 	// Routing
 	require(config.root.ROUTER_ROOT)(app, passport);
-	logger.info("TEST!!!!");
 	
 	// catch 404 and forward to error handler
 	app.use(function(req, res, next) {
