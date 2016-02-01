7cc30dbe4a8dae714409afde3d615c295d7ee7eb
Jaewoo Woo <gewoose@gmail.com>
2016-02-01 19:21:53 +0900

    Add logger (morgan, morganDaily, winston, winstonDaily)

--- setting/app.js
+++ setting/app.js
@@ -1,28 +1,37 @@
+var express = require('express');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var path = require('path');
+		
+var setting = require('./../setting')
+var passport = require('passport');
+var mongoose = require('mongoose');
+
 module.exports = function(app) {
 	
-	var express = require('express');
-	var favicon = require('serve-favicon');
-	var bodyParser = require('body-parser');
-	var cookieParser = require('cookie-parser');
-	var path = require('path');
-	
-	// Require logging modules
-	//var fs = require('fs');
-	//var morgan = require('morgan');
-	//var morganDaily = require('file-stream-rotator');
-	//var Logger = require('./bin/logger.js');
-	//global.logger = new Logger();
+	// app.set
 	app.set('port', config.app.port);
 	app.set('views', config.root.VIEW_ROOT);
 	app.set('view engine', 'ejs');
 	
+	// basic middleware
 	app.use(express.static(config.root.PUBLIC_ROOT));
 	app.use(favicon(path.join(config.root.PUBLIC_ROOT, 'favicon.ico')));
 	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(bodyParser.json());
 	app.use(cookieParser());
+
+	
+	// Middleware setting
+	setting.logger(app);
+	setting.mongoose(mongoose);
+	setting.passport(passport);
 	
 	
+	// Routing
+	require(config.root.ROUTER_ROOT)(app, passport);
+	logger.info("TEST!!!!");
 	
 	// catch 404 and forward to error handler
 	app.use(function(req, res, next) {
@@ -35,17 +44,5 @@ module.exports = function(app) {
 		res.status(err.status || 500);
 		res.send(err.message);
 	});
-
-		
-	var setting = require('./../setting')
-	var passport = require('passport');
-	var mongoose = require('mongoose');
-	
-	setting.mongoose(mongoose);
-	setting.passport(passport);
-	//setting.logger(logger);
-	
-	// Routing
-	require(config.root.ROUTER_ROOT)(app, passport);
 	
 };
--- setting/logger.js
+++ setting/logger.js
@@ -1,3 +1,41 @@
-module.exports = function(logger) {
+var morgan = require('morgan');
+var morganDaily = require('file-stream-rotator');
+var winston = require('winston');
+var winstonDaily = require('winston-daily-rotate-file');
+
+module.exports = function(app) {
+	
+	// Set morgan logger for web server
+	var filePattern = "[:date[iso]] :method :url :status (:response-time ms) - :res[content-length]";
+	app.use(morgan('dev'));
+	app.use(morgan(filePattern, {
+		skip: function (req, res) { return res.statusCode < 400 },
+		stream: morganDaily.getStream({
+			filename: config.root.APP_ROOT + "/log/smithoo_http(%DATE%).log",
+			frequency: "daily",
+			verbose: false,
+			date_format: "YYYY-MM-DD"
+		})
+	}));
+	
+	
+	// Set winston logger for service
+	var winstonLogger = new (winston.Logger)({
+		transports: [
+			new (winston.transports.Console) ({
+				level: 'debug'
+			}),
+			new winstonDaily ({
+				name: "service_log",
+				level: 'info',
+				json: false,
+				filename: config.root.APP_ROOT + "/log/smithoo_service",
+				datePattern: "(yyyy-MM-dd).log"
+			})
+		],
+		exitOnError: false
+	});
+	
+	global.logger = winstonLogger;
 	
 };
